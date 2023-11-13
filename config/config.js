cae87945708df9d13ed08cf464c5e8e4c6b47336
v4nessa04 <v4nessa04@gmail.com>
2023-11-13 16:26:44 -0500

    updated JAWSDB_URL

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 
 // create connection to our db
-const sequelize = process.env.JAWS_URL
-  ? new Sequelize(process.env.JAWS_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DATABASE_NAME, process.env.USER_NAME, process.env.USER_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
